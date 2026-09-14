36bda462f19a7710c275935bc76d639ea5e1b699
Luthetus <luthetus@gmail.com>
2026-09-14 12:27:00 -0400

    reduce 'class ByteList' to only contain the code that ends up being used

--- src/RendererFiles/listTypes.js
+++ src/RendererFiles/listTypes.js
@@ -12,13 +12,6 @@ class ByteList {
     }
 
     /**
-     * Does not clear the information, only sets 'this.count' to '0'.
-     */
-    clear() {
-        this.count = 0;
-    }
-
-    /**
      * TODO: ensure all the parameters are encoded, especially because I'm noticing myself forgetting.
      */
     insert(index, byte) {
@@ -31,105 +24,6 @@ class ByteList {
         this.bytes[index] = byte;
 
         this.count++;
-    }
-
-    insertString(index, string, encoder) {
-        this.ensureCapacityForInsertion(index, string.length);
-
-        if (index !== this.count) {
-            this.copyTo(this.bytes, index, this.bytes, index + string.length, this.count - index);
-        }
-
-        for (var i = 0; i < string.length; i++) {
-            this.bytes[index + i] = encoder.encode(string[i]);
-        }
-
-        this.count += string.length;
-    }
-    
-    /**
-     * @param {number} index 
-     * @param {Uint8Array} incomingBs the incoming bytes, name avoids confusion with this.bytes
-     * @param {number} offset the offset to begin reading from
-     * @param {number} length the amount of bytes to read
-     */
-    insertBytes(index, incomingBs, offset, length) {
-        this.ensureCapacityForInsertion(index, length);
-
-        if (index !== this.count) {
-            this.copyTo(this.bytes, index, this.bytes, index + length, this.count - index);
-        }
-
-        for (var i = 0; i < length; i++) {
-            this.bytes[index + i] = incomingBs[offset + i];
-        }
-
-        this.count += length;
-    }
-
-    /**
-     * 
-     * @param {number} sourceStart 
-     * @param {number} destinationStart 
-     * @param {number} length 
-     */
-    duplicateWithin(sourceStart, destinationStart, length) {
-
-        if (sourceStart + length > destinationStart) {
-            // TODO: This perhaps could result in the initial 'copyTo' step that creates space within the array, having clobbered the source.
-            //
-            // TODO: I'm gonna throw an error if 'sourceStart + length > destinationStart' that should let me do the simple duplicate case and then go from there.
-            //
-            // TODO: When copying text you only need to remember the positions maybe, and then if the user loses focus of the app...
-            // ...only then would you need to create text from their selection in case they intend to paste to an external app...
-            // ...otherwise paste could just be a copyWithin if copy and paste only occurs within the app itself?
-            //
-            throw new Error('TODO: sourceStart + length > destinationStart');
-        }
-
-        this.ensureCapacityForInsertion(destinationStart, length);
-
-        if (destinationStart !== this.count) {
-            this.copyTo(this.bytes, destinationStart, this.bytes, destinationStart + length, this.count - destinationStart);
-        }
-
-        this.copyTo(this.bytes, sourceStart, this.bytes, destinationStart, length);
-
-        this.count += length;
-    }
-
-    /**
-     * Does not clear trailing information.
-     * 
-     * count === 0 immediately returns
-     */
-    removeAt(index, count) {
-
-        if (index > this.count) { throw new Error('removeAt(...): index > this.count'); }
-        if (index + count > this.count) { throw new Error('removeAt(...): index + count > this.count'); }
-        if (count === 0) { return; }
-
-        if (index + count === this.count) {
-            let shiftableCount = this.count - (index + count);
-            if (shiftableCount > 0) {
-                this.copyTo(
-                    this.bytes,
-                    index + count,
-                    this.bytes,
-                    index,
-                    shiftableCount);
-            }
-        }
-        else {
-            this.copyTo(
-                this.bytes,
-                index + count,
-                this.bytes,
-                index,
-                this.count - (index + count));
-        }
-
-        this.count -= count;
     }
 
     ensureCapacityForInsertion(index, count) {

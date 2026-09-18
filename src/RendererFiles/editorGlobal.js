9b19afc65be580a5fb331ea2a675a80b7422f3a0
Luthetus <luthetus@gmail.com>
2026-09-18 18:10:15 -0400

    Update editorGlobal.js

--- src/RendererFiles/editorGlobal.js
+++ src/RendererFiles/editorGlobal.js
@@ -8997,6 +8997,6 @@ TODO: is this longest line logic everywhere it should be? (similar logic I mean,
 
 < ...
 
-
+None of you understand what I'm even saying either it's probably the most infuriating part of it all.
 
 */

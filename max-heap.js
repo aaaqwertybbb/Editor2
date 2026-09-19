64e4cf29d9700c6ab7c3129ca66c013b485d8d84
Luthetus <luthetus@gmail.com>
2026-09-19 09:41:43 -0400

    TODO: once you do this it completely invalidates...

--- max-heap.js
+++ max-heap.js
@@ -280,7 +280,9 @@ const maxHeap = new Uint32MaxHeap();
 maxHeap.peek()... expected answer is: (        3,      3)
 */
 
-
+// TODO: Actually pick a number that makes sense for the lineIndex, i.e.: it doesn't need 20 bits, presumably it'd be the shorter side.
+// TODO: once you do this it completely invalidates the fact that your line end position list can fit indices up to 32 bits.
+//       because you can only track up to (currently 20 bits given the exact way this is written at the moment).
 const LINE_BITS = 12;
 
 // 00000000000000000000000000000000

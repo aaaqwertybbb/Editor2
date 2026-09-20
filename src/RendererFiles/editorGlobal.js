f767086c68282cbac7ba65ac6b3250e4872ab4ed
Luthetus <luthetus@gmail.com>
2026-09-20 18:23:54 -0400

    Okay do the left movement such that if you hit the tab you stop the relative visual width

--- src/RendererFiles/editorGlobal.js
+++ src/RendererFiles/editorGlobal.js
@@ -9358,6 +9358,9 @@ Man I'm a bad programmer.
 A real baaaad programmer.
 Code is dirty...
 
+Okay do the left movement such that if you hit the tab you stop the relative visual width
+and just jump to the end column index then invoke a function that determines visual width from column index 0 to the landed at column index.
+
 
 
 */

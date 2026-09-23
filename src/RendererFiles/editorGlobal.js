cd24b5ec7a0c7c734eab295b2cff17cda1239a8f
Luthetus <luthetus@gmail.com>
2026-09-23 08:29:16 -0400

    Update editorGlobal.js

--- src/RendererFiles/editorGlobal.js
+++ src/RendererFiles/editorGlobal.js
@@ -10020,4 +10020,7 @@ Breakfast:
 Dinner:
 will be 1 lb of 98% fat free ground chicken with sriracha (100 grams of protein)
 
+I'm not eating donuts for breakfast anymore, at first I didn't notice anything bad but the more days of me doing it
+the worse I felt which I guess makes sense
+
 */

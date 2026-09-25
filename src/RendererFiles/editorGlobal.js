77871b7fbb2b7799f87af4189e8219b27af0838f
Luthetus <luthetus@gmail.com>
2026-09-25 08:42:05 -0400

    Update editorGlobal.js

--- src/RendererFiles/editorGlobal.js
+++ src/RendererFiles/editorGlobal.js
@@ -10158,6 +10158,8 @@ MyHouse.WAD it was a crazy realization
 Back in the day I viewed some of his MyHouse.WAD video but I never finished it.
 I watched that too a few days ago.
 
-
+Also I was laughing a bit while watching 'The Faculty' because elijah woods is in every movie I watch these days I feel like.
+I actually never watched movies in the past.
+I knew the 'vil vear vigs' interview but I never actually saw his movies he's all over.
 
 */
